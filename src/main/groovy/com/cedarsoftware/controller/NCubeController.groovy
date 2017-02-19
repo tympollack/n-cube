dea3f370efc2538f278980da3e0ce9ed9a574e57
jsnyder4 <jsnyder4@gaig.com>
2017-02-19 09:47:11 -0500

    Merge changes from n-cube-editor project

--- src/main/groovy/com/cedarsoftware/controller/NCubeController.groovy
+++ src/main/groovy/com/cedarsoftware/controller/NCubeController.groovy
@@ -113,13 +113,13 @@ class NCubeController extends BaseController
 
     protected String getUserForDatabase()
     {
-        Map<String, String> headers = [:]
+        Map<String, String> headers = new CaseInsensitiveMap<String, String>()
         String[] headerList = ['smuser','fakeuser','appid']
         HttpServletRequest request = JsonCommandServlet.servletRequest.get()
         Enumeration e = request.headerNames
         while (e.hasMoreElements())
         {
-            String headerName = (e.nextElement() as String).toLowerCase()
+            String headerName = (e.nextElement() as String)
             if (headerList.contains(headerName))
             {
                 headers[headerName] = request.getHeader(headerName)
@@ -130,11 +130,11 @@ class NCubeController extends BaseController
             }
         }
 
-        String realId = headers.containsKey('smuser') && !StringUtilities.isEmpty(headers['smuser']) ? headers['smuser'] : System.getProperty('user.name')
+        String realId = headers.containsKey('smuser') && StringUtilities.hasContent(headers['smuser']) ? headers['smuser'] : System.getProperty('user.name')
         nCubeService.userId = realId
 
-        if (headers.containsKey('fakeuser') && !StringUtilities.isEmpty(headers['fakeuser'])
-                && headers.containsKey('appid') && !StringUtilities.isEmpty(headers['appid']))
+        if (headers.containsKey('fakeuser') && StringUtilities.hasContent(headers['fakeuser'])
+                && headers.containsKey('appid') && StringUtilities.hasContent(headers['appid']))
         {
             String[] appIdParts = headers['appid'].split('~')
             if (appIdParts.length > 1)
@@ -1300,14 +1300,17 @@ class NCubeController extends BaseController
     void clearCache(ApplicationID appId)
     {
         appId = addTenant(appId)
-        if (!nCubeService.isAdmin(appId))
+        if (isAppAdmin(appId))
         {
-            return
+            nCubeService.clearCache(appId)
+            clearAppCache(appId.tenant)
+            clearVersionCache(appId.app)
+            clearBranchCache(appId)
         }
-        nCubeService.clearCache(appId)
-        clearAppCache(appId.tenant)
-        clearVersionCache(appId.app)
-        clearBranchCache(appId)
+        else if (!appId.head)
+        {
+            nCubeService.clearCache(appId)
+        }
     }
 
     void createBranch(ApplicationID appId)
